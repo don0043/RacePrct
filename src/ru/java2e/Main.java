131ec5dc1908dd207f2b534d0acaee74e8544c8e
don0043 <daniiar.arkabaev@iaau.edu.kg>
2020-05-09 16:40:20 +0600

    first commit

---
+++ src/ru/java2e/Main.java
@@ -0,0 +1,13 @@
+package ru.java2e;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Race");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1101, 600);
+        f.setVisible(true);
+
+    }
+}
+
