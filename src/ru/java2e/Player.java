ff1a5fb4fb1493eb92036429e970394008dd22ce
don0043 <daniiar.arkabaev@iaau.edu.kg>
2020-05-10 14:28:20 +0600

    now car can move commit 3

--- src/ru/java2e/Player.java
+++ src/ru/java2e/Player.java
@@ -2,25 +2,70 @@ package ru.java2e;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Player {
+
+    public static final int MAX_V = 100;
+    public static final int MAX_TOP = 10;
+    public static final int MAX_BOTTOM = 480;
+
     Image img = new ImageIcon("res/player.png").getImage();
 
-    int v = 4;
+    int v = 0;
     int dv = 0;
     int s = 0;
 
     int x = 30;
     int y = 100;
+    int dy = 0;
 
 
     int layer1 = 0;
+    int layer2 = 1200;
 
 
 
     public void move() {
         s += v;
-        layer1 -= v;
+        v += dv;
+        if (v <= 0) v = 0;
+        if (v >= MAX_V) v = MAX_V;
+        y -= dy;
+        if (y <= MAX_TOP) y = MAX_TOP;
+        if (y >= MAX_BOTTOM)y = MAX_BOTTOM;
+        if (layer2 - v <= 0) {
+            layer1 = 0;
+            layer2 = 1200;
+        } else {
+            layer1 -= v;
+            layer2 -= v;
 
+        }
     }
-}
+
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_RIGHT){
+            dv = 5;
+        }
+        if (key == KeyEvent.VK_LEFT ){
+            dv = -1;
+        }
+        if (key == KeyEvent.VK_UP){
+            dy = 5;
+        }
+        if (key == KeyEvent.VK_DOWN){
+            dy = -5;
+        }
+    }
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_LEFT){
+            dv = 0;
+        }
+        if (key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN){
+            dy = 0;
+    }
+}}
+
--- src/ru/java2e/Road.java
+++ src/ru/java2e/Road.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
@@ -16,11 +18,23 @@ public class Road extends JPanel implements ActionListener {
 
     public Road(){
         mainTimer.start();
+        addKeyListener(new MyKeyAdapter());
+        setFocusable(true);
+    }
+
+    private class MyKeyAdapter extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+            p.keyPressed(e);
+        }
+        public void keyReleased(KeyEvent e){
+            p.keyReleased(e);
+        }
     }
 
     public void paint(Graphics g) {
         g = (Graphics2D) g;
         g.drawImage(img, p.layer1, 0, null);
+        g.drawImage(img, p.layer2, 0, null);
         g.drawImage(p.img, p.x, p.y, null);
 
 
