fd209638a9ec37f211b82a550742fa9e4b08963f
don0043 <daniiar.arkabaev@iaau.edu.kg>
2020-05-10 16:47:26 +0600

    commit 11

--- src/ru/java2e/Main.java
+++ src/ru/java2e/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame f = new JFrame("Race");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(1101, 600);
+        f.setSize(1100, 600);
         f.add(new Road());
         f.setVisible(true);
 
